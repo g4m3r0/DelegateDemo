7c0225ba0a8c7e2eceeff135cbde401a4750b93b
Lucas Schmutzler <9322695+g4m3r0@users.noreply.github.com>
2023-10-24 23:53:08 +0200

    Add another delegate example

---
+++ ConsolePersonFilter/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace ConsolePersonFilter;
+
+class Program
+{
+    
+    public delegate bool FilterDelegate(Person p);
+    
+    
+    
+    static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>()
+        {
+            new Person() { Name = "Tim", Age = 12 },
+            new Person() { Name = "Bob", Age = 18 },
+            new Person() { Name = "Sue", Age = 33 },
+            new Person() { Name = "Mary", Age = 76 },
+        };
+        
+        Console.WriteLine("Hello People!");
+        
+        DisplayPeople("Children", people, IsMinor);
+        DisplayPeople("Adults", people, IsAdult);
+        DisplayPeople("Seniors", people, IsSenior);
+    }
+    
+    private static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+    {
+        Console.WriteLine(title);
+        
+        foreach (var person in people)
+        {
+            if (filter(person))
+            {
+                Console.WriteLine($"{person.Name} {person.Age}");
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    static bool IsMinor(Person person)
+    {
+        return person.Age < 18;
+    }
+    
+    static bool IsAdult(Person person)
+    {
+        return person.Age >= 18;
+    }
+
+    static bool IsSenior(Person person)
+    {
+        return person.Age >= 65;
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
